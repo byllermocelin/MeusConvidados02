b98951a9e31aa6c90d2dffa71f76fb61e425833b
Rodrigo Mocelin <byllermocelin@gmail.com>
2018-01-07 16:12:57 -0200

    s.10.72 Fragments

--- app/src/main/java/com/mocelin/meusconvidados02/MainActivity.java
+++ app/src/main/java/com/mocelin/meusconvidados02/MainActivity.java
@@ -2,6 +2,8 @@ package com.mocelin.meusconvidados02;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -64,16 +66,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+
+        Fragment fragment = null;
+        Class fragemntClass = null;
+
         int id = item.getItemId();
 
         if (id == R.id.nav_all_guests) {
-            // Handle the camera action
+            fragemntClass = AllInvitedFragment.class;
         } else if (id == R.id.nav_present) {
 
         } else if (id == R.id.nav_absent) {
 
         }
+
+        try {
+            fragment = (Fragment) fragemntClass.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.frame_content, fragment).commit();
+
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
